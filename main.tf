3218c9e44bf135b115a7ceb9c6dc288ab70197a4
naveen3607 <naveenreddykallam3607@gmail.com>
2023-08-26 12:15:15 -0400

    Terraform Script

--- main.tf
+++ main.tf
@@ -3,13 +3,6 @@ resource "aws_s3_bucket" "main" {
   bucket = var.bucket_name
 }
 
-resource "aws_s3_bucket_ownership_controls" "main" {
-  bucket = aws_s3_bucket.main.id
-  rule {
-    object_ownership = "BucketOwnerPreferred"
-  }
-}
-
 resource "aws_s3_bucket_public_access_block" "main" {
   bucket = aws_s3_bucket.main.id
 
@@ -21,7 +14,6 @@ resource "aws_s3_bucket_public_access_block" "main" {
 
 resource "aws_s3_bucket_acl" "main" {
   depends_on = [
-    aws_s3_bucket_ownership_controls.main,
     aws_s3_bucket_public_access_block.main,
   ]
 
