6670673db80cf72e2d4324f8b0cd4ee58f35446d
naveen3607 <naveenreddykallam3607@gmail.com>
2023-08-26 12:12:34 -0400

    Terraform Script

--- main.tf
+++ main.tf
@@ -1,4 +1,30 @@
 #Create S3 Bucket
-resource "aws_s3_bucket" "mybucket" {
+resource "aws_s3_bucket" "main" {
   bucket = var.bucket_name
+}
+
+resource "aws_s3_bucket_ownership_controls" "main" {
+  bucket = aws_s3_bucket.main.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "main" {
+  bucket = aws_s3_bucket.main.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "main" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.main,
+    aws_s3_bucket_public_access_block.main,
+  ]
+
+  bucket = aws_s3_bucket.main.id
+  acl    = "public-read"
 }
